3f6ec22ee94cb9cbef8105759b61b61e9cdd17c7
Mateus Nunes <mateusnunes9519@gmail.com>
2022-10-10 00:41:15 +0000

    ajustes no site

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import IconButton from '@material-tailwind/react/components/IconButton'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
@@ -7,17 +8,21 @@ import { GalerySection } from '../src/components/GalerySection'
 import { Header } from '../src/components/Header'
 import { NavBar } from '../src/components/NavBar'
 import styles from '../styles/Home.module.css'
+import ImgWhatsapp from '../assets/whatsapp.png'
 
 
 const Home: NextPage = () => {
   return (
-    <>
+    <div className='absolute w-full'>
+      <a href='https://web.whatsapp.com/send?phone=554299261950' className='fixed bottom-10  flex items-center justify-center right-10 md:right-24 z-10 rounded-full bg-[#229B4B] hover:bg-green-500 outline-none drop-shadow-none w-[50px] h-[50px]'>
+        <Image src={ImgWhatsapp}/>
+      </a>
       <NavBar/>
       <Header/>
       <AboutSection/>
       <GalerySection/>
       <Details/>
-    </>
+    </div>
   )
 }
 
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,19 +1,119 @@
 import Image from "next/image";
-import ImgLogo from '../../assets/logo.png'
+import ImgLogo from '../../assets/logoPortaldoSol.svg'
 import Link from "next/link";
+
+import { useState, useEffect } from "react";
+import {
+  Navbar,
+  MobileNav,
+  Typography,
+  Button,
+  IconButton,
+} from "@material-tailwind/react";
 
 
 export function NavBar() {
+
+    const [openNav, setOpenNav] = useState(false);
+ 
+  useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => window.innerWidth >= 960 && setOpenNav(false)
+    );
+  }, []);
+
+  const navList = (
+    <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-semibold"
+      >
+        <a href="#" className="flex items-center">
+          Home
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-semibold"
+      >
+        <a href="#" className="flex items-center">
+          Sobre
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-semibold"
+      >
+        <a href="#" className="flex items-center">
+          Contato
+        </a>
+      </Typography>
+    </ul>
+  );
+
     return (
-        <div className="flex-row items-center justify-around bg-slate-50 border-b-2 border-yellow-700 flex px-10" >
-                <div className="flex ">
-                    <Image width={130} height={93.13} src={ImgLogo}/>
-                </div>
-                <div className="flex items-center gap-8 text-md font-semibold">
-                    <Link href='/'>Home</Link>
-                    <Link href='/about'>Sobre</Link>
-                </div>
+        <nav className="mx-auto w-screen-xl py-1 px-4 border-b-2 border-yellow-700">
+        <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
+          <Image  src={ImgLogo}/>
+            <div className="hidden lg:block">{navList}</div>
+          <IconButton
+            variant="text"
+            className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+            ripple={false}
+            onClick={() => setOpenNav(!openNav)}
+          >
+            {openNav ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                className="h-6 w-6"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            )}
+          </IconButton>
         </div>
+        <MobileNav open={openNav}>
+          {navList}
+        </MobileNav>
+      </nav>
+        // <div className="flex-row items-center justify-around bg-slate-50 border-b-2 border-yellow-700 flex px-10" >
+        //         <div className="flex ">
+        //             <Image width={130} height={93.13} src={ImgLogo}/>
+        //         </div>
+        //         <div className="flex items-center gap-8 text-md font-semibold">
+        //             <Link href='/'>Home</Link>
+        //             <Link href='/about'>Sobre</Link>
+        //         </div>
+        // </div>
 
     )
 }
