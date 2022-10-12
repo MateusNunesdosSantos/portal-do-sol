cccfe06a657d09e429b804237273d83200f22d94
Mateus Nunes <mateusnunes9519@gmail.com>
2022-10-12 12:03:30 +0000

    ajustes

--- src/components/ButtonWhatsapp/index.tsx
+++ src/components/ButtonWhatsapp/index.tsx
@@ -5,7 +5,7 @@ import ImgWhatsappSvg from '../../../assets/whatsapp.svg'
 
 export function ButtonWhatsapp() {
     return (
-        <a  href='https://wa.me/5542988679697?text=Olá, tudo bem?' target="_blank"
+        <a  href='https://wa.me/5542988679697?text=Olá, tudo bem?'
             className='fixed bottom-10  flex items-center justify-center right-10 md:right-24 z-10 rounded-full bg-[#229B4B] hover:bg-green-500 outline-none drop-shadow-none w-[80px] h-[80px]'>
             <div className="w-10 h-10">
                 <Image src={ImgWhatsappSvg}/>
