de7c5ac79f1b94d6280853f087d6953df44609af
Mateus Nunes <mateusnunes9519@gmail.com>
2022-10-12 18:59:21 +0000

    ajustes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
 import IconButton from '@material-tailwind/react/components/IconButton'
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 import { AboutSection } from '../src/components/AboutSection'
 import { ButtonWhatsapp } from '../src/components/ButtonWhatsapp'
 import { Details } from '../src/components/Details'
@@ -11,13 +9,13 @@ import { Header } from '../src/components/Header'
 import { NavBar } from '../src/components/NavBar'
 import { Partnerships } from '../src/components/Partnerships'
 import { Contact } from '../src/components/Contacts'
-import styles from '../styles/Home.module.css'
+
 
 
 const Home: NextPage = () => {
   return (
     <div className='absolute w-full'>
-      <ButtonWhatsapp/>
+    
       <NavBar/>
       <Header/>
       <AboutSection/>
