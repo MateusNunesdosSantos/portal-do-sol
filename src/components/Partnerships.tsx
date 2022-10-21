54c2c842a914333f0f04c7a3175b6b00246ab3e3
Mateus Nunes <mateusnunes9519@gmail.com>
2022-10-21 01:24:22 +0000

    add parcerias

--- src/components/Partnerships.tsx
+++ src/components/Partnerships.tsx
@@ -1,6 +1,13 @@
 import LogoParcerias01 from '../../assets/logoEspacoSmart.svg'
 import LogoParcerias02 from '../../assets/logoComercial.svg'
 import LogoParcerias03 from '../../assets/logoLojasMM.svg'
+//import LogoParcerias04 from '../../assets/logo-abc.svg'
+import LogoParcerias05 from '../../assets/logo-deltacel.png'
+import LogoParcerias06 from '../../assets/logo-locatelli.png'
+import LogoParcerias07 from '../../assets/logo-Monatelli.png'
+
+
+
 import Image from 'next/image'
 
 export function Partnerships() {
@@ -8,10 +15,13 @@ export function Partnerships() {
     <section className="bg-amber-700">
       <div className="container p-6 mx-auto space-y-6 text-center lg:p-8 lg:space-y-8">
         <h2 className="text-3xl font-bold text-gray-900">Parcerias</h2>
-        <div className="flex gap-24 lg:gap-0 flex-wrap justify-center lg:justify-between">
+        <div className="container grid grid-cols-2 gap-8 p-4 mx-auto md:grid-cols-3 sm:grid-cols-2">
           <Image src={LogoParcerias01} />
           <Image src={LogoParcerias02} />
           <Image src={LogoParcerias03} />
+          <Image src={LogoParcerias05} />
+          <Image src={LogoParcerias06} />
+          <Image src={LogoParcerias07} />
         </div>
       </div>
     </section>
