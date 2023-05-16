8b8caed96f13d31da67874eb56c5b10d924cbce4
unknown <mateusnunes9519@gmail.com>
2023-05-16 06:10:41 -0300

    ajustes

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -12,7 +12,7 @@ export function Contact() {
             frameBorder="0"
             title="map"
             scrolling="no"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24494.784854415757!2d-50.78700243497363!3d-24.130612705141658!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ebd19edd3d411f%3A0xfd1b454a23b84c3!2sCondom%C3%ADnio%20Portal%20do%20Sol!5e0!3m2!1spt-BR!2sbr!4v1666128575555!5m2!1spt-BR!2sbr"
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14564.849333717844!2d-50.7763385!3d-24.1291802!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ebd1e4048ba03b%3A0x45c18e8b20f8f650!2sIate%20Clube%20Ortigueira!5e0!3m2!1spt-BR!2sbr!4v1684227856658!5m2!1spt-BR!2sbr"
           ></iframe>
         </div>
         <div className="container px-5 py-24 mx-auto flex">
@@ -24,3 +24,4 @@ export function Contact() {
     </>
   )
 }
+
