0428fcec38b173d2b9227fa2301f5caa1e2eae45
unknown <mateusnunes9519@gmail.com>
2023-09-19 21:30:24 -0300

    Alteração numero whatsapp de novo

--- src/components/ButtonWhatsapp/index.tsx
+++ src/components/ButtonWhatsapp/index.tsx
@@ -4,7 +4,7 @@ import ImgWhatsappSvg from '../../../assets/whatsapp.svg'
 export function ButtonWhatsapp() {
   return (
     <a
-      href="https://wa.me/5542920017044?text=Olá, Gostaria de saber informações sobre o IATE Clube"
+      href="https://wa.me/5542920017044?text=Olá, Gostaria de saber informações sobre o Iate Clube Ortigueira !"
       className="fixed bottom-10  animate-bounce flex items-center justify-center right-10 md:right-24 z-40 rounded-full bg-[#229B4B] hover:bg-green-500 outline-none drop-shadow-none w-[80px] h-[80px]"
     >
       <div className="w-10 h-10">
