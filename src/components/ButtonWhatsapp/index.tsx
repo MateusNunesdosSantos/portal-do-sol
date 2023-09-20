d57bc4fda4848dccf99f5232d9f538c1980e8802
unknown <mateusnunes9519@gmail.com>
2023-09-19 21:21:22 -0300

    Alteração numero whatsapp

--- src/components/ButtonWhatsapp/index.tsx
+++ src/components/ButtonWhatsapp/index.tsx
@@ -4,7 +4,7 @@ import ImgWhatsappSvg from '../../../assets/whatsapp.svg'
 export function ButtonWhatsapp() {
   return (
     <a
-      href="https://wa.me/5542988679697?text=Olá, tudo bem?"
+      href="https://wa.me/5542920017044?text=Olá, Gostaria de saber informações sobre o IATE Clube"
       className="fixed bottom-10  animate-bounce flex items-center justify-center right-10 md:right-24 z-40 rounded-full bg-[#229B4B] hover:bg-green-500 outline-none drop-shadow-none w-[80px] h-[80px]"
     >
       <div className="w-10 h-10">
